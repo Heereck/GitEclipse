757b42ffaeb924a93c947e02f1e0370ebb982ada
Erick Gutierrez <erick.alejandro@gmail.com>
2019-02-25 13:32:18 -0800

    Update TestClass.java
    multiplications

--- HelloWorld/src/TestClass.java
+++ HelloWorld/src/TestClass.java
@@ -1,8 +1,19 @@
 
 public class TestClass {
 	/**
-	 * comment
-	 *
-	 *
-	 */
+	 *Excercise 1 Github SDTE Training. 
+	 *Get the multiplications. 
+	 **/
+	public static void main(String[] args) {	
+		for (int i=1; i<=3;i++) {//initialize counters
+			
+			System.out.println("******** Table of "+i+" ********");
+			
+			for (int x=1; x<=3;x++) {
+				
+				System.out.println("Table of " +i+" is :"+i+" * "+x+"= " +i*x); //Get multiplication
+			}
+		}
+	}
+
 }
